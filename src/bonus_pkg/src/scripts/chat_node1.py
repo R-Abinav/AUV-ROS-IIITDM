35aa4c2bebeef84da29b5ca77a6f08af71064aab
Abinav <137711880+R-Abinav@users.noreply.github.com>
2024-08-23 17:09:03 +0530

    Update chat_node1.py

--- src/bonus_pkg/src/scripts/chat_node1.py
+++ src/bonus_pkg/src/scripts/chat_node1.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import String
+from bonus_pkg.msg import node1  
+from bonus_pkg.msg import node2   
 
-def callback(data):
-    # Check if the message is not from this node
-    if data._connection_header['callerid'] != rospy.get_name():
-        rospy.loginfo("[%s] Received: %s", rospy.get_name(), data.data)
+def callback(msg):
+    rospy.loginfo(f"Node1 received: {msg.data2}")
 
 def node1():
-    rospy.init_node('chat_node1', anonymous=True)
-    
-    pub = rospy.Publisher('chat_bot', String, queue_size=10)
-    rospy.Subscriber('chat_bot', String, callback)
-    
-    rate = rospy.Rate(10)  # 10hz
+    rospy.init_node('node1', anonymous=True)
+
+    pub = rospy.Publisher('chat', node1, queue_size=10)
+    rospy.Subscriber('chat', node2, callback)
+
+    rate = rospy.Rate(1)  # 1 Hz
     while not rospy.is_shutdown():
-        msg = input("[%s] Enter a message: " % rospy.get_name())
-        rospy.loginfo("[%s] Publishing: %s", rospy.get_name(), msg)
+        msg = node1()
+        msg.data1 = input("Node 1 is getting input")
+        rospy.loginfo("Node1 is sending: " + msg.data1)
         pub.publish(msg)
         rate.sleep()
 
